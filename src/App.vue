a2bffca7070da5c1408e760b0ddc34fe38b75f0e
Omar Abdullah <omar.abdullah@netigo.de>
2025-09-04 19:11:28 +0200

    Add image and website bindings to template

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
 <script setup>
 const message = "Hello World";
 const add = (x, y) => x + y;
+
+const website = "https://vuejs.org";
+
+const imgUrl = "https://placehold.co/600x400";
+const imgAlt = "Lorem ipsum";
+const imgWidth = 600;
+const imgHeight = 400;
 </script>
 
 <!-- Template structure -->
@@ -9,6 +16,18 @@ const add = (x, y) => x + y;
   <!-- Text interpolation -->
   <h1>{{ message }}</h1>
   <p>2 + 3 = {{ add(2, 3) }}</p>
+
+  <!-- attribute binding (old way)-->
+  <a v-bind:href="website" target="_blank">vuejs</a>
+
+  <br>
+
+  <!-- attribute binding (new way)-->
+  <a :href="website" target="_blank">vuejs</a>
+
+  <br>
+
+  <img :src="imgUrl" :alt="imgAlt" :width="imgWidth" :height="imgHeight">
 </template>
 
 <!-- Component specific styles -->
