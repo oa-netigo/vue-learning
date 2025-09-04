2d370c8491901df34aa735811c9da6d3dbc7a8bc
Omar Abdullah <omar.abdullah@netigo.de>
2025-09-04 19:02:28 +0200

    Add addition function and display result in template

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <!-- Javascript logic -->
 <script setup>
 const message = "Hello World";
+const add = (x, y) => x + y;
 </script>
 
 <!-- Template structure -->
 <template>
   <!-- Text interpolation (binding data) -->
   <h1>{{ message }}</h1>
+  <p>2 + 3 = {{ add(2, 3) }}</p>
 </template>
 
 <!-- Component specific styles -->
