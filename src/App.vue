feb436cd421ff374c7ee9cc133833ee4ecddfc05
Omar Abdullah <omar.abdullah@netigo.de>
2025-09-04 18:52:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<!-- Javascript logic -->
+<script setup>
+const message = "Hello World";
+</script>
+
+<!-- Template structure -->
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<!-- Component specific styles -->
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
